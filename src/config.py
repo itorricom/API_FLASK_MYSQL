c1f2cf62385773c6b56f8f971ae6d55254f02027
Isidoro Torrico M <itorrico.m@gmail.com>
2022-08-11 17:48:59 -0400

    Crud funcionando

---
+++ src/config.py
@@ -0,0 +1,15 @@
+from distutils.debug import DEBUG
+
+
+class DevelopmentConfig():
+    DEBUG=True
+
+    MYSQL_HOST= 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'api_flask'
+
+
+config = {
+    'development' : DevelopmentConfig
+}
